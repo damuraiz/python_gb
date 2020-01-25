311b920e0d4755295ac6f6e53441b1d14ba49f1f
Damir Yunusov <samurai.net@gmail.com>
2020-01-26 03:05:28 +0700

    задание 2.2

--- lesson2/task02_1.py
+++ lesson2/task02_1.py
@@ -3,7 +3,8 @@
 # Использовать функцию type() для проверки типа.
 # Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
 
-my_list = [1, 1.0, complex(0, 1), "1", [1], (1, 1), {1}, {1: 1}, True, b'1', bytearray(b'1'), None, ValueError(), print]
+my_list = [1, 1.0, complex(0, 1), "1", [1], (1, 1), {1}, {1: 1},
+           True, b'1', bytearray(b'1'), None, ValueError(), print, range(1)]
 
 for value in my_list:
     print(f"Элемент {value} имеет тип - {type(value)}")
