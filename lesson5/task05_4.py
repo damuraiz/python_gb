22fae810291f0224e54a985d0fb7e76dfac4d9c1
Damir Yunusov <samurai.net@gmail.com>
2020-02-07 01:11:58 +0700

    задание 5.4 подправил

---
+++ lesson5/task05_4.py
@@ -0,0 +1,23 @@
+# Создать (не программно) текстовый файл со следующим содержимым:
+# One — 1
+# Two — 2
+# Three — 3
+# Four — 4
+# Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
+# При этом английские числительные должны заменяться на русские.
+# Новый блок строк должен записываться в новый текстовый файл.
+
+en_ru = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+
+
+def translate(line, dictionary=en_ru):
+    for word_en, word_ru in dictionary.items():
+        line = line.replace(word_en, word_ru)
+    return line
+
+
+with open("task05_4.txt") as in_f:
+    lines = in_f.readlines()
+    with open("task05_4.temp.txt", "w") as out_f:
+        for line in lines:
+            print(translate(line).replace("\n", ""), file=out_f)
