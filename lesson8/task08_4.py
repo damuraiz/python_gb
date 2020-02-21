746c86e693cae1159f17220ed1c154bfecf1bf88
Damir Yunusov <samurai.net@gmail.com>
2020-02-21 08:11:27 +0700

    Задание 8.5-8.6

--- lesson8/task08_4.py
+++ lesson8/task08_4.py
@@ -4,17 +4,35 @@
 # В базовом классе определить параметры, общие для приведенных типов.
 # В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
 
+class ItemNotFoundError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
 class Warehouse:
     def __init__(self):
         self.inventory_counter = 0
-        self.items = []
+        self.items = {}
+        self.sended_items = {}
 
     def add(self, item):
         self.inventory_counter += 1
-        self.items.append((self.inventory_counter, item))
+        self.items[self.inventory_counter] = item
+
+    def send(self, number, department):
+        if self.sended_items.get(department) is None:
+            self.sended_items[department] = {}
+        if number not in self.items.keys():
+            raise ItemNotFoundError("Оргтехника с таким инвентарным номером не найдена")
+        self.sended_items[department][number] = self.items.pop(number)
 
     def __str__(self):
-        return '\n'.join([f'Инвентарный номер {num} - {str(item)}' for num, item in self.items])
+        result = '\n'.join([f'Инвентарный номер {num} - {str(item)}' for num, item in self.items.items()])
+        for dep in self.sended_items.keys():
+            result += '\n' + dep + '\n'
+            result += '\n'.join(
+                [f'Инвентарный номер {num} - {str(item)}' for num, item in self.sended_items[dep].items()])
+        return result
 
 
 class OfficeEquipment:
@@ -29,7 +47,7 @@ class OfficeEquipment:
 class Printer(OfficeEquipment):
     def __init__(self, brand, model, is_color):
         OfficeEquipment.__init__(self, brand, model)
-        self.is_color=is_color
+        self.is_color = is_color
 
 
 class Scanner(OfficeEquipment):
@@ -46,15 +64,26 @@ class Copier(Printer, Scanner):
         self.copy_speed = copy_speed
 
 
-
 warehouse = Warehouse()
 
-printer = Printer("HP", "LJ300", False)
-scanner = Scanner("Epson", "SD300", "4800x4800")
-copier = Copier("HP", "DJ5400", True, "4600x4600", 26)
+warehouse.add(Printer("HP", "LJ300", False))
+warehouse.add(Printer("HP", "LJ300", False))
+warehouse.add(Printer("HP", "LJ300", False))
+warehouse.add(Scanner("Epson", "SD300", "4800x4800"))
+warehouse.add(Scanner("Epson", "SD300", "4800x4800"))
+warehouse.add(Scanner("Epson", "SD300", "4800x4800"))
+warehouse.add(Copier("HP", "DJ5400", True, "4600x4600", 26))
+warehouse.add(Copier("HP", "DJ5400", True, "4600x4600", 26))
 
-warehouse.add(printer)
-warehouse.add(scanner)
-warehouse.add(copier)
+warehouse.send(1, "Python Development")
+warehouse.send(2, "Java Development")
+warehouse.send(4, "Python Development")
+warehouse.send(5, "Java Development")
+warehouse.send(8, "Reception")
+
+try:
+    warehouse.send(2, "Reception")
+except ItemNotFoundError as err:
+    print(err)
 
 print(warehouse)
